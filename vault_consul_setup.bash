7122ac748f86bdb0dc06931f3997ea0368f20e14
Ian Sanders <sandersi@me.com>
2021-01-26 15:12:39 -0600

    jenkins pipeline and addition of vault_consul_setup.bash script Tue Jan 26 03:12:39 PM CST 2021

---
+++ vault_consul_setup.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+set +x
+export VAULT_TOKEN=$(curl -s -k --request POST --data "{ \"role_id\": \"$ROLE_ID\", \"secret_id\": \"$SECRET_ID\"}" $VAULT_ADDR/v1/auth/approle/login | jq -r .auth.client_token)
+export CONSUL_HTTP_ADDR=$(vault kv get -format=json secret/ansible/consul_basic_read | jq -r .data.data.consul_http_addr)
+export CONSUL_HTTP_SSL=$(vault kv get -format=json secret/ansible/consul_basic_read | jq -r .data.data.consul_http_ssl)
+export CONSUL_HTTP_TOKEN=$(vault kv get -format=json secret/ansible/consul_basic_read | jq -r .data.data.token)
+export CONSUL_CACERT=$VAULT_CAPATH
